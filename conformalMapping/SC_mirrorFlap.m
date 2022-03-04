2d5a34990cdc0b8ba5ab6dd73b4a0ca8ad2d328f
Andreas Holm Akselsen <andreas.akselsen@sintef.no>
2022-03-04 14:39:20 +0100

    Conformal mapping for wavemaker.

---
+++ conformalMapping/SC_mirrorFlap.m
@@ -0,0 +1,53 @@
+clear all
+close all
+PLOT_OTHER_PLANES = 0;
+
+wbl = .6;
+wbOveWater = .1;
+thetaMax = 20*pi/180;
+L = .4;
+
+limZeta = [0.001,.7*wbl,-.5*wbl,.5*wbl];
+% limZeta = [0.001,2,-1,1];
+
+cLines = 10;
+% cLines = (0:.1:1)*.5*wbl;
+nPoints = 100;%500;
+
+dt = 0.1;
+tMax = 10;
+T = 1;
+nTerms = 50;
+
+
+% d = 1; % unit domain 
+% d = wbl + wbOveWater; % scale two domains
+
+
+zeta = linspace(limZeta(1),limZeta(2),nPoints) + 1i*linspace(limZeta(3),limZeta(4),nPoints)';
+
+hf = figure('color','w');
+hold on;
+t = 0;
+while t<tMax
+    theta = thetaMax*cos( 2*pi/T*t );
+    thp = theta/pi;
+
+%     H = hypergeometric2f1(1,1.5,1.5-thp,-(zeta/d).^2,100);
+%     fz = -tan(theta).*(d+gamma(-thp)./gamma(.5-thp).*sqrt(pi)/(pi-2*theta).*zeta.^(1-2*thp).*(zeta-1i*d).^thp.*(zeta+1i*d).^thp.*(1+(zeta/d).^2).*H);
+
+    c = -(wbl+wbOveWater)*tan(theta) + 1i*wbOveWater;
+    k = pi^1.5/(pi-2*theta).*(wbl-1i*c).*exp(-1i*theta)./(gamma(1+thp)*gamma(.5-thp));
+    d = L*real(k)*(L/wbl)^(-2*thp)*hypergeometric2f1(-thp,.5-thp,1.5-thp,-(L/wbl).^2,nTerms)/(L+wbl*tan(theta));   
+    fz = c + k*(zeta/d).^(1-2*thp).*hypergeometric2f1(-thp,.5-thp,1.5-thp,-(zeta/d).^2,nTerms);
+    
+    cla
+    contour(real(fz),imag(fz),real(zeta),cLines,'r','linewidth',1);
+    contour(real(fz),imag(fz),imag(zeta),cLines,'b','linewidth',1);
+    t = t+dt;
+    drawnow
+end
+
+% dat = load('FF_r.csv')+1i* load('FF_i.csv')
+
+
--- readCurrentCsv.m
+++ readCurrentCsv.m
@@ -1,21 +1,78 @@
-clear all
-
-csvFile = 'C:\Users\andreasak\SINTEF\OSC - Current system CFD simulations folder - Dokumenter\BasinDesignSep2021\CFD_results\4Csaba\Case000_UNS\XYZ_Internal_Table_1000.csv';
-tab = readtable(csvFile);
-
-assert( numel(unique(tab.Z_m_))==1, 'data not constant in z' );
+close all
 
 
 
-% [x,ii] = sort(tab.X_m_)
-% 
-% u = tab.Velocity_i__m_s_(ii);
-% 
-% x = tab.X_m_(ii);
-% 
-% z = tab.Z_m_(ii);
+if ~exist('siU','var')
+    csvFile = 'C:\Users\andreasak\SINTEF\OSC - Current system CFD simulations folder - Dokumenter\BasinDesignSep2021\CFD_results\4Csaba\Case000\XYZ_Internal_Table_Y=25_6000.csv';
+    tab = readtable(csvFile);
+    assert( numel(unique(tab.Y_m_))==1, 'data not constant in y' );
+    siU = scatteredInterpolant(tab.X_m_,tab.Z_m_-max(tab.Z_m_),tab.Velocity_i__m_s_,'linear','none');
+    siW = scatteredInterpolant(tab.X_m_,tab.Z_m_-max(tab.Z_m_),tab.Velocity_k__m_s_,'linear','none');
+end
+
+nx = 200;
+nz = 100;
+
+x = linspace(0,50,nx);
+z = linspace(0,-1,nz).';
 
 
-siU = scatteredInterpolant(tab.X_m_,tab.Y_m_,tab.Velocity_i__m_s_);
+% [xx,zz] = meshgrid(linspace(min(x),max(x),200),linspace(min(z),max(z),100));
+[xx,zz] = meshgrid(x,z);
+Usurf = siU(x,max(z)+0*x);
+Ulim = [min(Usurf),1.2*max(Usurf)];
+hf = figure('color','w','position',[2503 345 1236 576]);
+caxis(Ulim);
+subplot(2,1,1); contourf(xx,zz,siU(xx,zz),linspace(Ulim(1),Ulim(2),20));hold on
+colorbar;title('U')
+axis equal;
+subplot(2,1,2); contourf(xx,zz,siW(xx,zz),20);
+colorbar; title('W')
+axis equal;
 
-figure, imagesc(tab.X_m_,tab.Y_m_,tab.Velocity_i__m_s_)
+[phi1,phi2,psi1,psi2] = deal(zeros(size(xx)));
+
+xh = .5*(x(1:end-1)+x(2:end));
+zh = .5*(z(1:end-1)+z(2:end));
+dz = diff(z);
+dx = diff(x);
+phi1(:,1) = [0; cumsum(siW(x(1)+0*zh, zh).*dz,1)]; 
+phi1(:,2:nx) = phi1(:,1) + cumsum(siU(xh+0*z,z+0*xh).*dx,2); 
+
+psi1(:,1) = [0; cumsum(siU(x(1)+0*zh,zh).*dz,1)]; 
+psi1(:,2:nx) = psi1(:,1) - cumsum(siW(xh+0*z,z+0*xh).*dx,2); 
+
+phi2(1,:) = [0, cumsum(siU(xh,z(1)+0*xh).*dx,2)];
+phi2(2:nz,:) = phi2(1,:) + cumsum(siW(x+0*zh,zh+0*x).*dz,1);
+
+psi2(1,:) = [0, -cumsum(siW(xh,z(1)+0*xh).*dx,2)];
+psi2(2:nz,:) = phi2(1,:) + cumsum(siU(x+0*zh,zh+0*x).*dz,1);
+
+omega_j = nan(size(xx));
+xxc = xx(2:nz-1,2:nx-1);zzc = zz(2:nz-1,2:nx-1);
+omega_j(2:nz-1,2:nx-1) =  (siU(xxc,zz(3:nz,2:nx-1)) - siU(xxc,zz(1:nz-2,2:nx-1)))./(z(3:nz)-z(1:nz-2))...
+                        - (siW(xx(2:nz-1,3:nx),zzc) - siW(xx(2:nz-1,1:nx-2),zzc))./(x(3:nx)-x(1:nx-2));
+
+hf2 = figure('color','w','position',[2503 49 1236 872]);
+subplot(3,1,1);hold on;
+[~,hppji1] = contourf(xx,zz,phi1,20);
+contour(xx,zz,psi1,'k');
+caxis([min(phi1(:)),max(phi1(:))]);colorbar
+subplot(3,1,2);hold on
+% contourf(xx,zz,phi2,hppji1.LevelList);
+contourf(xx,zz,phi2,20);
+contour(xx,zz,psi2,'k');
+caxis([min(phi2(:)),max(phi2(:))]);colorbar
+subplot(3,1,3);hold on
+contourf(xx,zz,omega_j,linspace(-.2,.2,20));colorbar;
+
+ws = load('C:\gits\SFo_gitHOS\hosm-nwt2d\basinSpec\workspace_flipLinear_Lh50x1_nx1024nk512_dimVort15x3p5_Uj0p55_posZ1.mat');
+figure('color','w','position',[2503 345 1236 576])
+subplot(2,1,1); contourf(ws.xx,ws.zz,ws.u.',20);hold on
+colorbar;title('U')
+axis equal;
+ylim([-1,0])
+subplot(2,1,2); contourf(ws.xx,ws.zz,ws.w.',20);
+colorbar; title('W')
+axis equal;
+ylim([-1,0])
